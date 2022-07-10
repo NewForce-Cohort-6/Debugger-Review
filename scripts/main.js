2111affe8af507d764a68dea54e7aa11563aa541
sarahebrooks12 <sarahebrooks12@gmail.com>
2022-07-10 09:23:12 -0400

    debugger review

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,52 @@
+import { useZodiac } from "./database.js";
+
 console.log("Welcome to the main module")
+
+
+
+
+const htmlbox = document.querySelector("#boxybox")
+
+const zodiacInfo = useZodiac()
+// const writeToDOM = () => {
+// for( const obj of zodiacInfo){
+//   htmlbox.innerHTML += `<div>
+//   <ul id="zodiac--${obj.id}">
+//   <li>${obj.name}</li>
+//   <li>${obj.mascot}</li>
+//   <li>${obj.dates}</li>
+//   <li>${obj.element}</li>
+//   <li>${obj.wvAnimal}</li>
+//   </ul>
+//   </div>`
+
+// }
+// }
+
+htmlbox.innerHTML = zodiacInfo.map(obj => `<div>
+<ul id="zodiac--${obj.id}">
+<li>${obj.name}</li>
+<li>${obj.mascot}</li>
+<li>${obj.dates}</li>
+<li>${obj.element}</li>
+<li>${obj.wvAnimal}</li>
+</ul>
+</div>`).join("")
+
+// writeToDOM()
+
+document.addEventListener("click", (e) => {
+ 
+  if(+e.target.id.split("--")[1] === 4){
+    window.alert("I'm feeling Crabby")
+  }
+
+})
+// {
+//   id: 12,
+//   name: "Pisces",
+//   mascot: "Fish",
+//   dates: "February 19-March 20",
+//   element: "Water",
+//   wvAnimal: "White-Tailed Deer"
+// }
